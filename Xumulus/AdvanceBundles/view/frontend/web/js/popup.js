a4f051c90999abde6330d31f81ba783dba4ade50
Dipak Patil <dipak@elsner.com>
2017-11-30 20:08:43 +0530

    Changes done for showing popup with product desciption

---
+++ Xumulus/AdvanceBundles/view/frontend/web/js/popup.js
@@ -0,0 +1,13 @@
+require(['jquery'],function($){
+    $(document).ready(function() {        
+        $(".options-list img").hover(function(e){
+            $(".large").css("top",(e.pageY+2)+"px")
+                             .css("left",(e.pageX+2)+"px")                    
+                             .html("<img src="+ $(this).attr("alt") +" alt='Large Image'/><br/>")
+                             .fadeIn("slow");
+        }, function(){
+            $(".large").fadeOut("fast");
+        });        
+        
+    });
+});
