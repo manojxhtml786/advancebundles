13e849b3b2e3e4cd8a6c303d6a9fb47ccd00fde0
Dipak Patil <dipak@elsner.com>
2017-12-01 20:11:02 +0530

    Done CSS for the bundle product page and summary block chnaged JIRA ticket XE-18

---
+++ Xumulus/AdvanceBundles/Block/Catalog/Product/View/Type/Bundle/Header.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Xumulus\AdvanceBundles\Block\Catalog\Product\View\Type\Bundle;
+
+class Header extends \Magento\Framework\View\Element\Template 
+{
+    public $_objectManager;
+    public $_warrantyFactory;
+    
+    public function __construct(
+        \Magento\Framework\ObjectManagerInterface $objectmanager,
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        array $data = []
+    ) {
+        $this->_objectManager = $objectmanager;
+        $this->_registry = $registry;
+        parent::__construct($context, $data);
+    }
+
+    public function getProductImageUrl()
+    {
+        $imgUrl = $this->_objectManager->get('\Magento\Catalog\Helper\Image');
+        $image_url_medium = $imgUrl->init($this->getProduct(), 'product_page_image_small')->setImageFile($this->getProduct()->getFile())->resize(300,300)->getUrl();
+        return $image_url_medium;
+    }
+
+    public function getProduct()
+    {        
+        return $this->_registry->registry('current_product');
+    }    
+    
+}
